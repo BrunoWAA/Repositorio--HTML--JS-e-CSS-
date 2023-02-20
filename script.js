92ad961708763fe7bf5f110b9a664d96e42b275a
mobifilho <83096494+mobifilho@users.noreply.github.com>
2023-02-20 20:55:17 -0300

    2 commit

--- script.js
+++ script.js
@@ -16,5 +16,16 @@ function acoesBotoes ()
     {
         exibirHTML("Curriculo/curriculo.html", "div-principal");
     });
+
+    document.getElementById("btn-home").addEventListener("click", function() 
+    {
+        document.location.reload(true);
+    });
+
+    document.getElementById("btn-Loja").addEventListener("click", function() 
+    {
+        exibirHTML("Loja de Carros/veiculos.html", "div-principal");
+    });
+
 }
   
