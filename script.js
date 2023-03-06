a818a9424bdf24d5cf6b5835201ddc2173e7b190
mobifilho <83096494+BrunoWAA@users.noreply.github.com>
2023-03-05 22:20:32 -0300

    1commit do dia

--- script.js
+++ script.js
@@ -12,30 +12,36 @@ function exibirHTML(url, div)
 
 function acoesBotoes ()
 {
-    document.getElementById("btn-curriculo").addEventListener("click", function() 
-    {
-        exibirHTML("Curriculo/curriculo.html", "div-principal");
-    });
-
     document.getElementById("btn-home").addEventListener("click", function() 
     {
         document.location.reload(true);
     });
 
-    document.getElementById("btn-Loja").addEventListener("click", function() 
+    document.getElementById("menu-aula-1").addEventListener("change", function() 
     {
-        //
-        exibirHTML("Loja de Carros/veiculos.html", "div-principal");
-    });
+        select = document.getElementById("menu-aula-1").value
+        if(select == "Curriculo")
+        {
+            console.log("entrei no curriculo")
+            exibirHTML("Curriculo/curriculo.html", "div-principal")
+            
+        }
+        else if(select == "Loja")
+        {
+            console.log("entrei na Loja")
+            exibirHTML("Loja de Carros/veiculos.html", "div-principal");
+        }
+        else if(select == "Form")
+        {
+            console.log("entrei no Form")
+            exibirHTML("formulario/formulario.html", "div-principal");
+        }
+        else if(select == "Api")
+        {
+            console.log("entrei na API")
+            exibirHTML("Api/api.html", "div-principal");
+        }
 
-    document.getElementById("btn-form").addEventListener("click", function() 
-    {
-        exibirHTML("formulario/formulario.html", "div-principal");
     });
-    document.getElementById("btn-api").addEventListener("click", function() 
-    {
-        exibirHTML("Api/api.html", "div-principal");
-    });
-
 }
   
