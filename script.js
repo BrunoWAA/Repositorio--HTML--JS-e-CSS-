c5623e684c6ebb996d74365b8bec369e548a6af4
mobifilho <83096494+BrunoWAA@users.noreply.github.com>
2023-03-05 23:17:06 -0300

    .

--- script.js
+++ script.js
@@ -22,26 +22,35 @@ function acoesBotoes ()
         select = document.getElementById("menu-aula-1").value
         if(select == "Curriculo")
         {
-            console.log("entrei no curriculo")
             exibirHTML("Curriculo/curriculo.html", "div-principal")
             
         }
         else if(select == "Loja")
         {
-            console.log("entrei na Loja")
             exibirHTML("Loja de Carros/veiculos.html", "div-principal");
         }
         else if(select == "Form")
         {
-            console.log("entrei no Form")
             exibirHTML("formulario/formulario.html", "div-principal");
         }
         else if(select == "Api")
         {
-            console.log("entrei na API")
             exibirHTML("Api/api.html", "div-principal");
         }
 
     });
 }
-  
+document.getElementById("menu-aula-2").addEventListener("change", function() 
+{
+    select = document.getElementById("menu-aula-2").value
+    if(select == "Curriculo")
+    {
+        exibirHTML("Curriculo/curriculo.html", "div-principal")
+        
+    }
+    else if(select == "Loja")
+    {
+        exibirHTML("Loja de Carros/veiculos.html", "div-principal");
+    }
+
+});
